885602da89c9638687a26ac2e17c36b31cce99c1
LudaTverday <Ludachka22@gmail.com>
2023-01-06 14:43:52 +0200

    fixing cipher with big shift

--- shiftCipher.js
+++ shiftCipher.js
@@ -1,33 +1,34 @@
 "use strict";
 const A = "a".charCodeAt(0);
 const Z = "z".charCodeAt(0);
-const NUM_ABC = Z - A;
+const NUM_ABC = Z - A + 1;
 function shiftCipher(word, shift = 1) {
+    shift %= NUM_ABC;
     const arrLetters = word.split('');
     return arrLetters.reduce((res, symbol) => {
-        let codeSymbol = symbol.charCodeAt(0);
-        let shiftCode = codeSymbol + shift;
-        return shiftCipherSymbol(res, symbol, codeSymbol, shiftCode);
+        return shiftCipherSymbol(res, symbol, shift);
     }, "");
 }
 function shiftDeCipher(word, shift = 1) {
+    shift %= NUM_ABC;
+    shift *= -1;
     const arrLetters = word.split('');
     return arrLetters.reduce((res, symbol) => {
-        let codeSymbol = symbol.charCodeAt(0);
-        let shiftCode = codeSymbol - shift;
-        return shiftCipherSymbol(res, symbol, codeSymbol, shiftCode);
+        return shiftCipherSymbol(res, symbol, shift);
     }, "");
 }
-function shiftCipherSymbol(res, sym, codeSymbol, shiftCode) {
+function shiftCipherSymbol(res, sym, shift) {
+    let codeSymbol = sym.charCodeAt(0);
+    let shiftCode = codeSymbol + shift;
     if (codeSymbol < A || codeSymbol > Z) {
         res += sym;
     }
     else {
         if (shiftCode < A) {
-            res += String.fromCharCode(shiftCode + (NUM_ABC + 1));
+            res += String.fromCharCode(shiftCode + NUM_ABC);
         }
         else if (shiftCode > Z) {
-            res += String.fromCharCode(shiftCode - (NUM_ABC + 1));
+            res += String.fromCharCode(shiftCode - NUM_ABC);
         }
         else {
             res += String.fromCharCode(shiftCode);
@@ -36,12 +37,20 @@ function shiftCipherSymbol(res, sym, codeSymbol, shiftCode) {
     return res;
 }
 let word = "abz.1+";
-let step = 3;
+let step = 27;
 let wordCipher = shiftCipher(word, step);
 let wordDeCipher = shiftDeCipher(wordCipher, step);
 console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher}, AFTER_Decipher: ${wordDeCipher}`);
 step = -3;
 let wordCipher1 = shiftCipher(word, step);
-let wordDeCipher1 = shiftDeCipher(wordCipher, step);
+let wordDeCipher1 = shiftDeCipher(wordCipher1, step);
 console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher1}, AFTER_Decipher: ${wordDeCipher1}`);
+step = 150;
+let wordCipher2 = shiftCipher(word, step);
+let wordDeCipher2 = shiftDeCipher(wordCipher2, step);
+console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher2}, AFTER_Decipher: ${wordDeCipher2}`);
+step = 1000;
+let wordCipher3 = shiftCipher(word, step);
+let wordDeCipher3 = shiftDeCipher(wordCipher3, step);
+console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher3}, AFTER_Decipher: ${wordDeCipher3}`);
 //# sourceMappingURL=shiftCipher.js.map
--- shiftCipher.ts
+++ shiftCipher.ts
@@ -1,33 +1,34 @@
 const A = "a".charCodeAt(0);
 const Z = "z".charCodeAt(0);
-const NUM_ABC = Z - A;
+const NUM_ABC = Z - A + 1;
 
 function shiftCipher(word: string, shift: number = 1): string {
+    shift %= NUM_ABC;
     const arrLetters = word.split('');
-    return arrLetters.reduce((res, symbol) => {
-        let codeSymbol = symbol.charCodeAt(0);
-        let shiftCode = codeSymbol + shift;
-        return shiftCipherSymbol(res, symbol, codeSymbol, shiftCode);
+    return arrLetters.reduce((res:string, symbol:string) => {
+        return shiftCipherSymbol(res, symbol, shift);
     }, "");
 }
 
 function shiftDeCipher(word: string, shift: number = 1): string {
+    shift %= NUM_ABC;
+    shift *= -1;
     const arrLetters = word.split('');
-    return arrLetters.reduce((res, symbol) => {
-        let codeSymbol = symbol.charCodeAt(0);
-        let shiftCode = codeSymbol - shift;
-        return shiftCipherSymbol(res, symbol, codeSymbol, shiftCode);
+    return arrLetters.reduce((res: string, symbol: string) => {
+        return shiftCipherSymbol(res, symbol, shift);
     }, "");
 }
 
-function shiftCipherSymbol(res: string, sym:string, codeSymbol: number, shiftCode: number): string {
+function shiftCipherSymbol(res: string, sym: string, shift: number): string {
+    let codeSymbol: number = sym.charCodeAt(0);
+    let shiftCode: number = codeSymbol + shift;
     if (codeSymbol < A || codeSymbol > Z) {
         res += sym;
     } else {
         if (shiftCode < A) {
-            res += String.fromCharCode(shiftCode + (NUM_ABC + 1));
+            res += String.fromCharCode(shiftCode + NUM_ABC);
         } else if (shiftCode > Z) {
-            res += String.fromCharCode(shiftCode - (NUM_ABC + 1));
+            res += String.fromCharCode(shiftCode - NUM_ABC);
         }
         else {
             res += String.fromCharCode(shiftCode);
@@ -36,14 +37,24 @@ function shiftCipherSymbol(res: string, sym:string, codeSymbol: number, shiftCod
     return res;
 }
 
-let word = "abz.1+";
-let step = 3;
+let word: string = "abz.1+";
+let step: number = 27;
 
-let wordCipher = shiftCipher(word, step);
-let wordDeCipher = shiftDeCipher(wordCipher, step);
+let wordCipher: string = shiftCipher(word, step);
+let wordDeCipher: string = shiftDeCipher(wordCipher, step);
 console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher}, AFTER_Decipher: ${wordDeCipher}`);
 
 step = -3;
 let wordCipher1 = shiftCipher(word, step);
-let wordDeCipher1 = shiftDeCipher(wordCipher, step);
+let wordDeCipher1 = shiftDeCipher(wordCipher1, step);
 console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher1}, AFTER_Decipher: ${wordDeCipher1}`);
+
+step = 150;
+let wordCipher2 = shiftCipher(word, step);
+let wordDeCipher2 = shiftDeCipher(wordCipher2, step);
+console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher2}, AFTER_Decipher: ${wordDeCipher2}`);
+
+step = 1000;
+let wordCipher3 = shiftCipher(word, step);
+let wordDeCipher3 = shiftDeCipher(wordCipher3, step);
+console.log(`Word: ${word}, STEP: ${step} AFTER_Cipher: ${wordCipher3}, AFTER_Decipher: ${wordDeCipher3}`);
