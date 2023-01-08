0ada12a0351a8e0ae0e25105e98464c4fbfdacef
LudaTverday <Ludachka22@gmail.com>
2023-01-08 20:39:30 +0200

    CW#31 TypeScript Basics

---
+++ app.js
@@ -0,0 +1,44 @@
+"use strict";
+const aCodeASCII = 'a'.charCodeAt(0);
+const zCodeASCII = 'z'.charCodeAt(0);
+const nEnglishLetters = zCodeASCII - aCodeASCII + 1;
+function shiftCipher(str, shift = 1) {
+    return cipherDecipher(str, shift, mapperCipher);
+}
+function shiftDecipher(str, shift = 1) {
+    return cipherDecipher(str, shift, mapperDecipher);
+}
+function cipherDecipher(str, shift, mapperFun) {
+    //  const arStr: string[] = Array.from(str);
+    const arStr = Array.from(str);
+    const arRes = arStr.map(symb => {
+        let res = symb;
+        if (symb <= 'z' && symb >= 'a') {
+            res = mapperFun(symb, shift);
+        }
+        return res;
+    });
+    return arRes.join('');
+}
+function mapperCipher(symb, shift) {
+    const actualShift = (symb.charCodeAt(0) - aCodeASCII + shift) % nEnglishLetters;
+    return String.fromCharCode(aCodeASCII + actualShift);
+}
+function mapperDecipher(symb, shift) {
+    const actialShift = (zCodeASCII - symb.charCodeAt(0) + shift) % nEnglishLetters;
+    return String.fromCharCode(zCodeASCII - actialShift);
+}
+function testCipherDecipher(data, testName) {
+    console.log(`${"*".repeat(10)}${testName}${"*".repeat(10)}`);
+    const funForTest = testName === "cipherTest" ? shiftCipher : shiftDecipher;
+    data.forEach(obj => console.log(`str=${obj.str}, shift=${obj.shift || 1} => ${funForTest(obj.str, obj.shift)}`));
+}
+const dataForCipherTest = [
+    { str: 'abc' }, { str: 'abz', shift: 1000 }
+];
+testCipherDecipher(dataForCipherTest, "cipherTest");
+const dataForDecipherTest = [
+    { str: 'bcd' }, { str: 'mnl', shift: 1000 }
+];
+testCipherDecipher(dataForDecipherTest, "decipherTest");
+//# sourceMappingURL=app.js.map
