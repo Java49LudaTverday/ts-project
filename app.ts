4b7713c2374f71af18148ec1c7e1bd9318929c77
LudaTverday <Ludachka22@gmail.com>
2023-01-10 23:34:43 +0200

    implements const in app.ts

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 import { CipherDecipher } from "./CipherDecipher";
 
-const cipher1 = new CipherDecipher (48,57,20);
+// const leastASCIIcode:number = 48;
+// const mostASCIIcode: number = 57;
+// const SHIFT:number = 20;
+
+const leastASCIIcode:number = "0".charCodeAt(0);
+const mostASCIIcode: number = "9".charCodeAt(0);
+const SHIFT:number = 5; 
+
+const cipher1 = new CipherDecipher (leastASCIIcode,mostASCIIcode,SHIFT);
 let res: string = cipher1.shiftCipher("abz123");
 console.log(cipher1);
 console.log(res);
